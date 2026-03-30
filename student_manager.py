a4e31691f47c7efd6b74b8b668b18e48650b168e
confidence <confidencenkeonye@gmail.com>
2026-03-30 13:30:00 +0100

    Implement search functionality, add functionality, view functionality for students records

---
+++ student_manager.py
@@ -0,0 +1,64 @@
+from storage import load_students, save_students
+
+# Add Student
+
+def add_student():
+    students = load_students()
+
+    student_id = input("Enter Student ID: ").strip()
+
+    # Check for duplicate ID 
+    for student in students:
+        if student["id"] == student_id:
+            print("❌ Student ID already exists.")
+            return
+        
+    name = input("Enter Student Name: ").strip()
+
+    try:
+        age = int(input("Enter Student Age: "))
+        score = float(input("Enter Student Score: "))
+    except ValueError:
+        print("❌ Invalid input. Age must be a number, score must be numeric.")
+        return
+    
+    new_student = {
+        "id": student_id,
+        "name": name, 
+        "age": age,
+        "score": score
+    }
+    
+    students.append(new_student)
+    save_students(students)
+
+    print("✅ Student added successfully.")
+
+# View All Students
+
+def view_students():
+    students = load_students()
+
+    if not students:
+        print("No students found.")
+        return
+    print("\n Student List:")
+    for student in students:
+        print(f"{student['id']} | {student['name']} | Age: {student['age']} | Score: {student['score']}")
+
+
+# Search Student by ID 
+
+def search_student():
+    students = load_students()
+
+    student_id = input("Enter Student ID to search: ").strip()
+
+    for student in students:
+        if student["id"] == student_id:
+            print("\n ✅ Student Found: ")
+            print(student)
+            return
+    
+    print("❌ Student not found.")
+
